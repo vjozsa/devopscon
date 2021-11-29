75bcb4d21c5fe12ecfffb7ee9407caca6d959dd7
Nir Koren <nir.koren@sap.com>
2021-11-29 12:19:49 +0100

    change string

--- build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
+++ build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
@@ -3,7 +3,7 @@ package org.nirkoren.maven.demo;
 public class HelloHandler {
 
 	public String sayHello() {
-		String hello = "Welcome, is it working from your laptop?";
+		String hello = "Welcome, is it working from your Laptop?";
 		return hello;
 	}
 
